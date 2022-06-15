bc052c635a9d2a47731e6b042a2b11b5cf3b4c1d
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-06-15 19:38:50 +0300

    feat: add basic server structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { runTest } from "./utils/test.js";
+import http from "http";
 
-console.log("Starting...");
+const server = http.createServer((req, res) => {
+  res.writeHead(404, { contentType: "application/json" });
+  res.end(JSON.stringify({ message: "Unknown route" }));
+});
+console.log("Starting  ...");
 runTest("After");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
